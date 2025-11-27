3e1221a2e753f3d78139f8c6a99891e4429a9f55
leonelmattiasuribe-tech <leonelmattiasuribe@gmail.com>
2025-11-26 21:05:36 -0400

    Update ajuste_mmc.py
    final: Cálculo de errores, generación de gráfica y reporte

--- src/ajuste_mmc.py
+++ src/ajuste_mmc.py
@@ -25,3 +25,32 @@ if __name__ == "__main__":
     A_calc, B_calc = calcular_coeficientes_mmc(X_test, Y_test)
     
     print(f"Resultado de prueba: A={A_calc:.2f}, B={B_calc:.2f}")
+
+calcular_coeficientes_mmc
+
+def calcular_errores_mmc(X, Y, A, B):
+
+    Calcula los errores estándar (delta) de los coeficientes A y B.
+    
+    N = len(X)
+    
+    # 1  Valores predichos y residuos
+    Y_modelo = A * X + B
+    residuos = Y - Y_modelo
+    
+    # 2 Desviación estándar de los residuos (Sr)
+    Sr_cuadrado = np.sum(residuos**2) / (N - 2)
+    Sr = np.sqrt(Sr_cuadrado)
+    
+    # 3 Denominador común
+    sum_X2 = np.sum(X**2)
+    sum_X = np.sum(X)
+    Den = N * sum_X2 - sum_X**2
+    
+    # 4 Error de la Pendiente (Delta A)
+    delta_A = Sr * np.sqrt(N / Den)
+    
+    # 5 Error del Intercepto (Delta B)
+    delta_B = Sr * np.sqrt(sum_X2 / Den)
+    
+    return delta_A, delta_B
